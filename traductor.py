283cc205328307edf53505873d8eb5d0a75586e0
joaquinxtomas <jtppalacios@gmail.com>
2023-10-29 16:06:00 -0300

    functions modules

--- traductor.py
+++ traductor.py
@@ -1,22 +1,5 @@
-from googletrans import Translator
-
-translator = Translator()
-
-dec = input("""Desde que idioma a que idioma desea traducir?
-            1-Inglés al español
-            2-Español al inglés
-            ------------------------------------------------
-            """)
-
-if(dec =="1"):
-    to_translate = input("Ingrese el texto que desea traducir: ")
-    trlted = translator.translate(to_translate, src="en", dest="es")
-    print(f'Traducción: {trlted.text}')
-elif(dec == "2"):
-    to_translate = input("Ingrese el texto que desea traducir: ")
-    trlted = translator.translate(to_translate,src="es",dest="en")
-    print(f'Traducción: {trlted.text}')
-else:
-    print("Opción inválida.")
-    
-
+def show_languages(list):
+    print("Idiomas disponibles: \n")
+    for i, language in enumerate(list, start = 1):
+        print(f'{i}-{language}')
+    print("--------------------------------------")
--- translator.py
+++ translator.py
@@ -1,14 +1,12 @@
 from googletrans import Translator
+import traductor as tc
 
 translator = Translator()
 
 lang = ("Ingles", "Español", "Portugués", "Mandarín", "Francés")
-cod_lang = ["en", "es", "pt","zh","fr"]
+cod_lang = ["en", "es", "pt","zh-tw","fr"]
 
-print("Idiomas disponibles: \n")
-for i, language in enumerate(lang, start = 1):
-    print(f'{i}-{language}')
-print("--------------------------------")
+tc.show_languages(lang)
 
 lang_1= input(f"Ingrese el numero del idioma de origen del texto: ")
 
@@ -17,10 +15,7 @@ if lang_1.isnumeric() and len(lang) > 0:
     code_language = cod_lang[int(lang_1)-1]
 
 
-print("Idiomas disponibles: \n")
-for i, language in enumerate(lang, start = 1):
-    print(f'{i}-{language}')
-print("--------------------------------")
+tc.show_languages(lang)
 
 to_translate = input("Ingrese el numero del idioma al que desea traducirlo:")
 
